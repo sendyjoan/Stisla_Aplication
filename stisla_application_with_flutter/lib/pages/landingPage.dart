92e588a9c79709837d9292208a5abd1b1d75377b
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-12-12 19:59:10 +0700

    Create landingPage.dart

---
+++ stisla_application_with_flutter/lib/pages/landingPage.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:stisla_application_with_flutter/pages/registerPage.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 1), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => Register()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration:
+                const BoxDecoration(color: Color.fromARGB(255, 0, 0, 0)),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 50.0,
+                      backgroundImage: AssetImage('assets/images/logo.jpg'),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10.0)),
+                    Text(
+                      "Aplikasi Stisla Android",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 24.0,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
