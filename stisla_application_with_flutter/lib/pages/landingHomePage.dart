2a92c97c1c1fec27cb50c610a00aeacdeb9335b5
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-12-13 18:40:28 +0700

    Create landingHomePage.dart

---
+++ stisla_application_with_flutter/lib/pages/landingHomePage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:stisla_application_with_flutter/pages/homePage.dart';
+// import 'package:stislaflutter/screen/homePage/profil/mainProfil.dart';
+
+// import 'kategori/mainKategori.dart';
+
+class LandingHome extends StatefulWidget {
+  const LandingHome({super.key});
+  @override
+  State<LandingHome> createState() => _LandingHomeState();
+}
+
+class _LandingHomeState extends State<LandingHome> {
+  int currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.list),
+            label: 'Kategori',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.accessibility),
+            label: 'Profil',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        const MainHome(),
+        // const MainKategori(),
+        // const MainProfil(),
+      ][currentPageIndex],
+    );
+  }
+}
