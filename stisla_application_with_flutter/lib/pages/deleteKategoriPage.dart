7bc6603f652a69023e021b3349b6c42d86d701e7
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-12-15 22:33:59 +0700

    Create deleteKategoriPage.dart

---
+++ stisla_application_with_flutter/lib/pages/deleteKategoriPage.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:stisla_application_with_flutter/api/crud_helper.dart';
+import 'package:stisla_application_with_flutter/model/kategori_model.dart';
+
+// ignore: must_be_immutable
+class DeleteCategori extends StatefulWidget {
+  Category category;
+  DeleteCategori({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  @override
+  State<DeleteCategori> createState() => _DeleteCategoriState();
+}
+
+class _DeleteCategoriState extends State<DeleteCategori> {
+  final TextEditingController txtEditCategory = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    txtEditCategory.text = widget.category!.name;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          16,
+        ),
+      ),
+      title: const Text('Delete Category'),
+      content: SizedBox(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: txtEditCategory,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 2, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 2, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Task',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              primary: const Color(0xff2da9ef),
+            ),
+            onPressed: () {
+              doDeleteCategory();
+            },
+            child: const Text(
+              'Delete',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  doDeleteCategory() async {
+    final response = await CrudHelper().deleteCategori(widget.category!);
+    print(response.body);
+    Navigator.pushNamed(context, "/main");
+  }
+}
